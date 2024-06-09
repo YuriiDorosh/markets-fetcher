288f170768403f516b73fc7e0afa4f2dde0f598a
Yurii <yuriydorosh2005@gmail.com>
2024-06-09 19:16:52 +0300

    UPDATE: Dmarket(api: added status_controller)

--- backend/services/dmarket/app/api.py
+++ backend/services/dmarket/app/api.py
@@ -1,7 +1,10 @@
 from flask import Flask
 from app.controllers.skin_controller import skin_controller
+from app.controllers.status_controller import status_controller
+
 
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(skin_controller)
+    app.register_blueprint(status_controller)
     return app
